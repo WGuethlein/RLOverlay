809c517eb81810c82d18a03df301e3984e6346b5
Wyatt Guethlein <112825572+WGuethlein@users.noreply.github.com>
2023-09-08 06:57:50 -0400

    Minor Cleanup in index.html

--- src/components/Players/Team.jsx
+++ src/components/Players/Team.jsx
@@ -4,7 +4,7 @@ import styles from "./styles/Team.module.css"
 const Team = (props) => {
     console.log(props)
     return(
-        <div className={styles.team}>
+        <div className={`${styles.team} ${props.id == "left" ? styles.left : styles.right}`}>
             <Player team={props.id} {...props.players[0]}/>
             <Player team={props.id} {...props.players[1]}/>
             <Player team={props.id} {...props.players[2]}/>
